ffac11d6435be63654b2d0c8068b33f70c7cdcbb
Andy Rennard <rennard@mac.com>
2014-12-16 15:49:54 +0000

    Initial version

--- MDTest/ViewController.swift
+++ MDTest/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var progressView: MDProgressView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,9 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
-
+	override func viewDidAppear(animated: Bool) {
+		self.progressView.animateIndeterminate()
+	}
+	
 }
 
