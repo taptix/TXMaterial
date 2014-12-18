f8b0c6aacbf1d041af0b8cdf865b2554855b7413
Andy Rennard <rennard@mac.com>
2014-12-18 16:07:50 +0000

    Amination tweaks
    Changes to demo project.

--- MDTest/ViewController.swift
+++ MDTest/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
 	@IBOutlet var progressViews: [ActivityProgressView]!
 	@IBOutlet weak var progressView: ActivityProgressView!
 	
+	@IBOutlet weak var slider: UISlider!
+	@IBOutlet weak var animateSwitch: UISwitch!
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -27,8 +30,7 @@ class ViewController: UIViewController {
 
 	
 	@IBAction func setProgress(sender: AnyObject) {
-		let slider = sender as UISlider
-		self.progressView.progress = slider.value
+		self.progressView.setProgress(self.slider.value, animated: false)
 	}
 
 	@IBAction func toggleAction(sender: AnyObject) {
@@ -38,16 +40,24 @@ class ViewController: UIViewController {
 	}
 
 	@IBAction func set0(sender: AnyObject) {
-		self.progressView.setProgress(0.0, animated: true)
+		self.progressView.setProgress(0.0, animated: self.animateSwitch.on)
+		self.slider.setValue(0.0, animated: self.animateSwitch.on)
 	}
 	@IBAction func set25(sender: AnyObject) {
-		self.progressView.setProgress(0.25, animated: true)
+		self.progressView.setProgress(0.25, animated: self.animateSwitch.on)
+		self.slider.setValue(0.25, animated: self.animateSwitch.on)
 	}
 	@IBAction func set50(sender: AnyObject) {
-		self.progressView.setProgress(0.50, animated: true)
+		self.progressView.setProgress(0.50, animated: self.animateSwitch.on)
+		self.slider.setValue(0.5, animated: self.animateSwitch.on)
+	}
+	@IBAction func set75(sender: AnyObject) {
+		self.progressView.setProgress(0.75, animated: self.animateSwitch.on)
+		self.slider.setValue(0.75, animated: self.animateSwitch.on)
 	}
 	@IBAction func set100(sender: AnyObject) {
-		self.progressView.setProgress(1.0, animated: true)
+		self.progressView.setProgress(1.0, animated: self.animateSwitch.on)
+		self.slider.setValue(1.0, animated: self.animateSwitch.on)
 	}
 }
 
--- TXMaterialDesign/ActivityProgressView.swift
+++ TXMaterialDesign/ActivityProgressView.swift
@@ -14,16 +14,21 @@ import UIKit
 	@IBInspectable public var isIndeterminate : Bool = false
 	@IBInspectable public var alwaysAnimate : Bool = false
 
+	private let strokeStartAnimationKey = "progressAnimationStrokeStart"
+	private let strokeEndAnimationKey = "progressAnimationStrokeEnd"
+	private  let rotationAnimationKey = "progressAnimationRotation"
+	
 	public var isAnimating : Bool = false{
 		didSet{
 			self.animate()
 		}
 	}
 	public var progress : Float = 0.0 {
-		didSet{
+		willSet{
 			self.isIndeterminate = false
-			self.shapeLayer.strokeStart = CGFloat(self.progress / 2.0)
-			self.shapeLayer.strokeEnd = CGFloat(self.progress)
+			self.removeAnimations()
+		}
+		didSet{
 			if self.isAnimating{
 				self.stopAnimating()
 			}
@@ -52,20 +57,23 @@ import UIKit
 	
 	public func setProgress(progress: Float, animated: Bool){
 		self.progress = progress
+		self.shapeLayer.strokeStart = CGFloat(self.progress / 2.0)
+		self.shapeLayer.strokeEnd = CGFloat(self.progress)
+
 		if	animated{
 			let timingFunction = CAMediaTimingFunction(controlPoints: 0.5, 0.0, 0.5, 1.0)
 			let presentationLayer = self.shapeLayer.presentationLayer() as CAShapeLayer
 			let progressAnim = CABasicAnimation(keyPath: "strokeEnd")
 			progressAnim.fromValue = presentationLayer.strokeEnd
 			progressAnim.toValue = CGFloat(self.progress)
-			progressAnim.duration = 2.0
+			progressAnim.duration = 1.0
 			progressAnim.timingFunction = timingFunction
 			self.shapeLayer.addAnimation(progressAnim, forKey: "setStrokeEnd")
 			
 			let progressAnim2 = CABasicAnimation(keyPath: "strokeStart")
 			progressAnim2.fromValue = presentationLayer.strokeStart
 			progressAnim2.toValue = CGFloat(self.progress / 2.0)
-			progressAnim2.duration = 2.0
+			progressAnim2.duration = 1.0
 			progressAnim2.timingFunction = timingFunction
 			self.shapeLayer.addAnimation(progressAnim2, forKey: "setStrokeStart")
 		}
@@ -104,10 +112,6 @@ import UIKit
 	}
 
 	private func animate(){
-		let strokeStartAnimationKey = "progressAnimationStrokeStart"
-		let strokeEndAnimationKey = "progressAnimationStrokeEnd"
-		let rotationAnimationKey = "progressAnimationRotation"
-		
 		if self.isAnimating{
 
 			if self.isIndeterminate{
@@ -142,14 +146,19 @@ import UIKit
 			
 		}
 		else {
-			let keys = [strokeEndAnimationKey, strokeStartAnimationKey, rotationAnimationKey]
-			for key in keys {
-				if let animation = self.shapeLayer.animationForKey(key){
-					self.shapeLayer.removeAnimationForKey(key)
-				}
-			}
-			self.shapeLayer.transform = CATransform3DMakeRotation(CGFloat(-M_PI/2.0), 0.0, 0.0, 1.0)
-
+			self.removeAnimations()
 		}
 	}
+
+	private func removeAnimations(){
+		let keys = [strokeEndAnimationKey, strokeStartAnimationKey, rotationAnimationKey]
+		for key in keys {
+			if let animation = self.shapeLayer.animationForKey(key){
+				self.shapeLayer.removeAnimationForKey(key)
+			}
+		}
+		self.shapeLayer.transform = CATransform3DMakeRotation(CGFloat(-M_PI/2.0), 0.0, 0.0, 1.0)
+		
+	}
+
 }
